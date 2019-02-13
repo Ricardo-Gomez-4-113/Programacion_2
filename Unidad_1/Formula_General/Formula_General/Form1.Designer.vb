bd090febaa0d276ff57175d8386603bb0bdba668
Ricardo-Gomez-4-113 <ricardogomez.rg517@gmail.com>
2019-02-13 10:56:40 -0600

    Formula

---
+++ Unidad_1/Formula_General/Formula_General/Form1.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.E1 = New System.Windows.Forms.Label()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.L2 = New System.Windows.Forms.Label()
+        Me.L3 = New System.Windows.Forms.Label()
+        Me.L4 = New System.Windows.Forms.Label()
+        Me.L5 = New System.Windows.Forms.Label()
+        Me.L6 = New System.Windows.Forms.Label()
+        Me.L7 = New System.Windows.Forms.Label()
+        Me.L8 = New System.Windows.Forms.Label()
+        Me.L9 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'E1
+        '
+        Me.E1.AutoSize = True
+        Me.E1.Font = New System.Drawing.Font("Microsoft Uighur", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.E1.Location = New System.Drawing.Point(33, -2)
+        Me.E1.Name = "E1"
+        Me.E1.Size = New System.Drawing.Size(120, 27)
+        Me.E1.TabIndex = 0
+        Me.E1.Text = "Formula General"
+        '
+        'B1
+        '
+        Me.B1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B1.Location = New System.Drawing.Point(194, 75)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(75, 23)
+        Me.B1.TabIndex = 1
+        Me.B1.Text = "Calcular"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'B2
+        '
+        Me.B2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.B2.Location = New System.Drawing.Point(197, 166)
+        Me.B2.Name = "B2"
+        Me.B2.Size = New System.Drawing.Size(75, 23)
+        Me.B2.TabIndex = 2
+        Me.B2.Text = "Salir"
+        Me.B2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 72)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 120)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(11, 166)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'L2
+        '
+        Me.L2.AutoSize = True
+        Me.L2.Location = New System.Drawing.Point(72, 53)
+        Me.L2.Name = "L2"
+        Me.L2.Size = New System.Drawing.Size(14, 13)
+        Me.L2.TabIndex = 6
+        Me.L2.Text = "A"
+        '
+        'L3
+        '
+        Me.L3.AutoSize = True
+        Me.L3.Location = New System.Drawing.Point(72, 104)
+        Me.L3.Name = "L3"
+        Me.L3.Size = New System.Drawing.Size(14, 13)
+        Me.L3.TabIndex = 7
+        Me.L3.Text = "B"
+        '
+        'L4
+        '
+        Me.L4.AutoSize = True
+        Me.L4.Location = New System.Drawing.Point(72, 150)
+        Me.L4.Name = "L4"
+        Me.L4.Size = New System.Drawing.Size(14, 13)
+        Me.L4.TabIndex = 8
+        Me.L4.Text = "C"
+        '
+        'L5
+        '
+        Me.L5.AutoSize = True
+        Me.L5.Location = New System.Drawing.Point(194, 12)
+        Me.L5.Name = "L5"
+        Me.L5.Size = New System.Drawing.Size(58, 13)
+        Me.L5.TabIndex = 9
+        Me.L5.Text = "Resultado:"
+        '
+        'L6
+        '
+        Me.L6.AutoSize = True
+        Me.L6.Location = New System.Drawing.Point(35, 37)
+        Me.L6.Name = "L6"
+        Me.L6.Size = New System.Drawing.Size(0, 13)
+        Me.L6.TabIndex = 10
+        '
+        'L7
+        '
+        Me.L7.AutoSize = True
+        Me.L7.Location = New System.Drawing.Point(162, 37)
+        Me.L7.Name = "L7"
+        Me.L7.Size = New System.Drawing.Size(0, 13)
+        Me.L7.TabIndex = 11
+        '
+        'L8
+        '
+        Me.L8.AutoSize = True
+        Me.L8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.L8.Location = New System.Drawing.Point(12, 37)
+        Me.L8.Name = "L8"
+        Me.L8.Size = New System.Drawing.Size(26, 13)
+        Me.L8.TabIndex = 12
+        Me.L8.Text = "X1:"
+        '
+        'L9
+        '
+        Me.L9.AutoSize = True
+        Me.L9.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.L9.Location = New System.Drawing.Point(139, 37)
+        Me.L9.Name = "L9"
+        Me.L9.Size = New System.Drawing.Size(26, 13)
+        Me.L9.TabIndex = 13
+        Me.L9.Text = "X2:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.L9)
+        Me.Controls.Add(Me.L8)
+        Me.Controls.Add(Me.L7)
+        Me.Controls.Add(Me.L6)
+        Me.Controls.Add(Me.L5)
+        Me.Controls.Add(Me.L4)
+        Me.Controls.Add(Me.L3)
+        Me.Controls.Add(Me.L2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.E1)
+        Me.Name = "Form1"
+        Me.Text = "Formula General"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents E1 As System.Windows.Forms.Label
+    Friend WithEvents B1 As System.Windows.Forms.Button
+    Friend WithEvents B2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents L2 As System.Windows.Forms.Label
+    Friend WithEvents L3 As System.Windows.Forms.Label
+    Friend WithEvents L4 As System.Windows.Forms.Label
+    Friend WithEvents L5 As System.Windows.Forms.Label
+    Friend WithEvents L6 As System.Windows.Forms.Label
+    Friend WithEvents L7 As System.Windows.Forms.Label
+    Friend WithEvents L8 As System.Windows.Forms.Label
+    Friend WithEvents L9 As System.Windows.Forms.Label
+
+End Class
---
+++ Unidad_1/Formula_General/Formula_General/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Dim a As Double
+    Dim b As Double
+    Dim c As Double
+    Dim raiz As Double
+    Dim raiz2 As Double
+    Dim resultado As Double
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles E1.Click
+
+    End Sub
+
+    Private Sub B2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B2.Click
+        End
+    End Sub
+
+    Private Sub B1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles B1.Click
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = TextBox3.Text
+        resultado = (b * b - (4 * a * c))
+        raiz = ((-b) + Math.Sqrt(resultado)) / (2 * a)
+        raiz2 = ((-b) - Math.Sqrt(resultado)) / (2 * a)
+        L8.Text = raiz
+        L9.Text = raiz2
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+       
+    End Sub
+
+    Private Sub L9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles L9.Click
+
+    End Sub
+End Class
